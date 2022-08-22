77b8870bd4fceff863e5b4c7a6ea4b6391e60312
sahaniraja <rstechnology2810@gmail.com>
2022-08-22 17:19:16 +0530

    Initial Commit

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
-console.log(process.env.API_KEY)
+
+const app = express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Welcome To ShopSpecify Coming Soon')
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port,()=>console.log(`Listening on port ${port}`));
